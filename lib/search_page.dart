5bbe995388f5a5e1c5917227cdc5a06a383f2999
frozen <791142033@qq.com>
2025-04-04 13:50:36 +0200

    search history logic

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'route_page.dart';
 import 'api/place.dart';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -8,20 +11,54 @@ class SearchPage extends StatefulWidget {
 }
 
 class SearchPageState extends State<SearchPage> {
-  final List<String> recentSearches = [
-    "Porta Nuova",
-    "Porta Susa",
-    "Esselunga",
-    "Polytechnic University of Turin",
-    "McDonald's",
-    "Torino Airport",
-    "IKEA"
-  ];
-
+  List<Map<String, dynamic>> recentSearches = [];
   List<Map<String, dynamic>> searchResults = [];
   bool isLoading = false;
   String? errorMessage;
   final TextEditingController _searchController = TextEditingController();
+  Timer? _debounce;
+  static const String _recentSearchesKey = 'recent_searches';
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+    _loadRecentSearches();
+  }
+
+  // 加载搜索记录
+  Future<void> _loadRecentSearches() async {
+    final prefs = await SharedPreferences.getInstance();
+    final searchesJson = prefs.getString(_recentSearchesKey);
+    if (searchesJson != null) {
+      setState(() {
+        recentSearches = List<Map<String, dynamic>>.from(json.decode(searchesJson));
+      });
+    }
+  }
+
+  Future<void> _saveRecentSearches() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_recentSearchesKey, json.encode(recentSearches));
+  }
+
+  void _onSearchChanged() {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      _searchPlaces(_searchController.text);
+    });
+  }
+
+  void _addToRecentSearches(Map<String, dynamic> place) {
+    setState(() {
+      recentSearches.removeWhere((item) => item['name_en'] == place['name_en']);
+      recentSearches.insert(0, place);
+      if (recentSearches.length > 10) {
+        recentSearches = recentSearches.sublist(0, 10);
+      }
+    });
+    _saveRecentSearches();
+  }
 
   Future<void> _searchPlaces(String query) async {
     if (query.isEmpty) {
@@ -68,9 +105,6 @@ class SearchPageState extends State<SearchPage> {
                 ),
                 prefixIcon: Icon(Icons.search),
               ),
-              onChanged: (query) {
-                _searchPlaces(query);
-              },
             ),
           ),
           if (isLoading)
@@ -83,17 +117,19 @@ class SearchPageState extends State<SearchPage> {
                 itemCount: searchResults.isEmpty ? recentSearches.length : searchResults.length,
                 itemBuilder: (context, index) {
                   if (searchResults.isEmpty) {
+                    final place = recentSearches[index];
                     return ListTile(
                       leading: Icon(Icons.history),
-                      title: Text(recentSearches[index]),
+                      title: Text(place['name_en']),
+                      subtitle: Text(place['name_it'] ?? ''),
                       onTap: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
                             builder: (context) => RoutePage(
-                              startName: recentSearches[index],
-                              endName: "Torino Airport",
-                              startCoord: [7.705189, 45.068828],
+                              startName: "Your location",
+                              endName: place['name_en'],
+                              startCoord: [place['Longitude'], place['Latitude']],
                               endCoord: [7.657668, 45.065126],
                             ),
                           ),
@@ -107,12 +143,13 @@ class SearchPageState extends State<SearchPage> {
                       title: Text(place['name_en']),
                       subtitle: Text(place['name_it'] ?? ''),
                       onTap: () {
+                        _addToRecentSearches(place);
                         Navigator.push(
                           context,
                           MaterialPageRoute(
                             builder: (context) => RoutePage(
-                              startName: place['name_en'],
-                              endName: "Torino Airport",
+                              startName: "Your location",
+                              endName: place['name_en'],
                               startCoord: [place['Longitude'], place['Latitude']],
                               endCoord: [7.657668, 45.065126],
                             ),
@@ -131,7 +168,9 @@ class SearchPageState extends State<SearchPage> {
 
   @override
   void dispose() {
+    _searchController.removeListener(_onSearchChanged);
     _searchController.dispose();
+    _debounce?.cancel();
     super.dispose();
   }
 }
